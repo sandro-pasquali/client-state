5b36145ddb320d416c61f8120fcae96e50e25eab
Sandro Pasquali <sandro.pasquali@twintechs.com>
2016-07-06 16:53:36 -0400

    better error handling, ++tests, ++docs

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 Manage event-emitting clients using a state machine.
 
-# install
+# Install
 
 `npm install client-state`
 
-# Usage
+# Description
 
 The goal is to provide a Finite State Machine that reflects various states of availability for a given endpoint or service (which I will call a `client`, such as a Redis database. 
 
@@ -14,12 +14,12 @@ An instance of this module's interface emits events corresponding to events in t
 
 For example, assume that a `client` emits the following events:
 
-- connect (a connection has been established)
-- ready (the connection is ready to receive commands)
-- reconnecting 
-- end (a connection has been lost)
-- error (some sort of error with the connection has occurred)
-- warning (some non-critical warning has been emitted by the client)
+- `connect` (a connection has been established)
+- `ready` (the connection is ready to receive commands)
+- `reconnecting` 
+- `end` (a connection has been lost)
+- `error` (some sort of error with the connection has occurred)
+- `warning` (some non-critical warning has been emitted by the client)
 
 If you pass this `client` to `client-state`:
 
@@ -42,5 +42,91 @@ stateM.on('ready', data => {
 });
 ```
 
+# Configuring FSM transitions
+
+The events given above are the default events. You can probably use this set for most remote client implementations. 
+
+If your target client supports the same concepts but not the same naming (likely), you can pass an event map to do translations.
+
+For example, if your db client emits `connected` rather than `connect` and `closed` rather than `end`, add the following to your config:
+
+```
+let cs = require('client-state');
+let stateM = cs(someclient, {
+    eventMap : {
+        'connected' : 'connect',
+        'closed' : 'end'
+    }
+});
+```
+
+If you would like to change the default FSM event model, you will need to define an FSM transition table. This is the default transition table:
+
+```
+[
+    { name: '_start', from: ['none'], to: 'start' },
+    { name: '_connect', from: ['end', 'start', 'reconnecting', 'error', 'warning'], to: 'connect' },
+    { name: '_ready', from: ['connect'], to: 'ready' },
+    { name: '_reconnecting', from: ['connect', 'ready', 'error', 'warning', 'end'], to: 'reconnecting' },
+    { name: '_end', from: ['connect', 'start', 'reconnecting', 'error', 'ready', 'warning'], to: 'end' },
+    { name: '_error', from: ['connect', 'start', 'reconnecting', 'error', 'ready', 'warning', 'end'], to: 'error' },
+    { name: '_warning', from: ['connect', 'start', 'reconnecting', 'error', 'ready', 'warning', 'end'], to: 'warning' }
+]
+```
+
+The FSM for `client-state` is implemented using [`javascript-state-machine`](https://github.com/jakesgordon/javascript-state-machine). Consult [that repo's documentation](https://github.com/jakesgordon/javascript-state-machine#usage) for information on how to define a transition table.
+
+To pass a new transition table:
+
+```
+let cs = require('client-state');
+let stateM = cs(someclient, {
+    transitions : [ ...your transition table ]
+});
+```
+
+# Transition handlers
+
+In addition to listening for transition events, you may also add handlers to run when entering, or leaving, a state.
+
+For example, if you would like to run a handler whenever the `ready` state is entered:
+
+```
+let cs = require('client-state');
+let stateM = cs(someclient, {
+    ready : data => {
+        // See below for description of #data
+    },
+});
+```
+
+Or when `ready` is left using the `leave` prefix (NOTE: *not* camel-cased):
+
+```
+let cs = require('client-state');
+let stateM = cs(someclient, {
+    leaveready : data => {
+        // See below for description of #data
+    },
+});
+```
+
+You can listen for *all* `leave` and `enter` states with `leavestate` and `enterstate`:
+
+```
+let cs = require('client-state');
+let stateM = cs(someclient, {
+    enterstate : data => {
+        // See below for description of #data
+    },
+    leavestate : data => {
+        // See below for description of #data
+    }
+});
+```
+
+# Context
+
+# Example
 
 The [`redis`](https://github.com/NodeRedis/node_redis) module emits all of the above events.
--- test/spec/suite.js
+++ test/spec/suite.js
@@ -18,14 +18,15 @@ module.exports = function(test, Promise) {
         // This is expected to throw a warning. See below.
         //
         let client = require('redis-hook')({
+            //url: 'http://foo.bar',
             password: 'thiscausesawarning'
-        });
+        })
 
         let stateM = state(client, {
             ready: data => {
 
                 // Note that this will throw an error, and that is expected.
-                // See #stateError handler
+                // See tests in on('warning') below, checking #stateError.
                 //
                 x=y
 
@@ -34,22 +35,27 @@ module.exports = function(test, Promise) {
             enterstate : data => {
                 var e = data.event;
                 scratch.evLog.push(e.from, e.to);
-                debug(`Enter event -> ${e.name} : ${e.from} -> ${e.to} > ${e.msg}`);
+                debug(`Enter event -> ${JSON.stringify(data)}`);
             },
             leavestate : data => {
                 var e = data.event;
                 scratch.evLog.push(e.from, e.to);
-                debug(`Leave event -> ${e.name} : ${e.from} -> ${e.to} > ${e.msg}`);
+                debug(`Leave event -> ${JSON.stringify(data)}`);
+            },
+
+            context : {
+                foo: 'isbar'
             }
-        }, {
-            foo: 'isbar'
         });
 
-        // Will catch errors occurring in state handlers (above)
-        //
-        stateM.on('stateError', errObj => {
-            scratch.stateError = errObj.msg;
+        stateM.on('error', e => {
+            debug(`CLIENT ERROR ${JSON.stringify(e)}`)
         });
+
+        stateM.on('stateError', e => {
+            debug(`State Error: ${JSON.stringify(e)}`);
+            scratch.stateError = e.event.msg;
+        })
 
         stateM.on('leaveconnect', data => {
             scratch.connectLeft = true;
@@ -60,7 +66,7 @@ module.exports = function(test, Promise) {
             test.equal(stateM.context(), data.context, 'instance.context() correctly returning context');
             test.equal(stateM.state(), 'warning', 'State machine in correct `warning` state');
             test.equal(scratch.connectLeft, true, '#leaveconnect fired');
-            test.ok(scratch.stateError, 'Correctly catching handler errors');
+            test.equal(scratch.stateError, 'y is not defined', 'Correctly catching handler errors');
             test.equal(data.event.msg, 'Warning: Redis server does not require a password, but a password was supplied.', 'Correctly warned about unnecessary #password');
 
             test.deepEqual(scratch.evLog, [
@@ -79,26 +85,64 @@ module.exports = function(test, Promise) {
         })
     })
 
-
-
     .then(() => new Promise(resolve => {
 
+        // Different log for this section
+        //
+        scratch.evLog = [];
+        scratch.stateErrors = [];
+
         let stateM = state(require('redis-hook')({}), {
-            ready: data => {
+
+            connect : data => {
+
                 return {
                     bleep : 'boop'
                 }
+            },
+            ready: data => {
+
+                // Create two state errors:
+                // 1. Inappropriate state change request
+                // 2. Attempt to modify immutable context (test that context is immutable)
+                // See 'ready' hander, below.
+                //
+                stateM.start();
+                data.context.blat = 'bleet';
+            },
+            enterstate : data => {
+                var e = data.event;
+                scratch.evLog.push(e.from, e.to);
+                debug(`Enter event -> ${JSON.stringify(data)}`);
+            },
+            leavestate : data => {
+                var e = data.event;
+                scratch.evLog.push(e.from, e.to);
+                debug(`Leave event -> ${JSON.stringify(data)}`);
+            },
+
+            context: {
+                beep: 'bop'
             }
-        }, {
-            beep: 'bop'
         });
 
         stateM.on('ready', data => {
 
+            test.deepEqual(scratch.stateErrors, [ 'event _start inappropriate in current state ready',
+                'Can\'t add property blat, object is not extensible' ], 'Correctly catching bad transition errors and context mutation errors');
             test.deepEqual(data.context, { bleep: 'boop', beep: 'bop'}, 'State change handlers correctly updating context');
 
             resolve();
-        })
+        });
+
+        stateM.on('stateError', e => {
+            debug(`State Error: ${JSON.stringify(e)}`);
+            scratch.stateErrors.push(e.event.msg);
+        });
+
+        stateM.on('error', e => {
+            debug(`CLIENT ERROR ${JSON.stringify(e)}`);
+        });
 
         /*
          // This is expected to throw a warning. See below.
@@ -114,7 +158,7 @@ module.exports = function(test, Promise) {
 
     P.timeout(1000)
     .catch(Promise.TimeoutError, err => {
-        P[1](`Timed out. Probably did not reach 'warning' state.\n Scratch: ${JSON.stringify(scratch)}`);
+        P[1](`Timed out. Probably did not reach end state.\n Scratch: ${JSON.stringify(scratch)}`);
     })
     .catch(err => chalk.bgRed.black(err.message));
 
